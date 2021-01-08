970b8a4dd13b07f43ddde9702d845a3053271706
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-01-07 22:58:21 -0600

    init project angularJS

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('my-app-angular app is running!');
+    expect(await page.getTitleText()).toEqual('angular-typescript app is running!');
   });
 
   afterEach(async () => {
